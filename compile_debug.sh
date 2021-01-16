10845174514557da71d6739965b429d204e9caa6
Marco Hladik <marco@icculus.org>
2021-01-16 22:47:31 +0100

    compile_debug: don't assume nproc exists either.

--- compile_debug.sh
+++ compile_debug.sh
@@ -1,1 +1,9 @@
-cmake -G "Unix Makefiles" -H. -Bbuild -DCMAKE_BUILD_TYPE=Debug && cmake --build build -- -j$(nproc)
+#!/bin/sh
+
+if ! [ -x "$(command -v nproc)" ]; then
+	BUILD_PROC=1
+else
+	BUILD_PROC=$(nproc)
+fi
+
+cmake -G "Unix Makefiles" -H. -Bbuild -DCMAKE_BUILD_TYPE=Debug && cmake --build build -- -j $BUILD_PROC
